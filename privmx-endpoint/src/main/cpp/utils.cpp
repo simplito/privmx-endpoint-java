47555cc7d8d1e17d3c98b071b4122cdaad619367
Dawid Jenczewski <djenczewski@simplito.com>
2025-02-18 14:17:14 +0100

    feat(privmx-endpoint): implement JniContextUtils::callResultEndpointApi and JniContextUtils::callVoidEndpointApi methods

--- privmx-endpoint/src/main/cpp/utils.cpp
+++ privmx-endpoint/src/main/cpp/utils.cpp
@@ -110,3 +110,54 @@ bool JniContextUtils::nullCheck(void *value, std::string value_name) {
     }
     return false;
 }
+
+template<typename T>
+void JniContextUtils::callResultEndpointApi(T *result, const std::function<T()> &fun) {
+    try {
+        *result = fun();
+    } catch (const privmx::endpoint::core::Exception &e) {
+        _env->Throw(coreException2jthrowable(e));
+    } catch (const IllegalStateException &e) {
+        _env->ThrowNew(
+                _env->FindClass("java/lang/IllegalStateException"),
+                e.what()
+        );
+    } catch (const std::exception &e) {
+        _env->ThrowNew(
+                _env->FindClass(
+                        "com/simplito/java/privmx_endpoint/model/exceptions/NativeException"),
+                e.what()
+        );
+    } catch (...) {
+        _env->ThrowNew(
+                _env->FindClass(
+                        "com/simplito/java/privmx_endpoint/model/exceptions/NativeException"),
+                "Unknown exception"
+        );
+    }
+}
+
+void JniContextUtils::callVoidEndpointApi(const std::function<void()> &fun) {
+    try {
+        fun();
+    } catch (const privmx::endpoint::core::Exception &e) {
+        _env->Throw(coreException2jthrowable(e));
+    } catch (const IllegalStateException &e) {
+        _env->ThrowNew(
+                _env->FindClass("java/lang/IllegalStateException"),
+                e.what()
+        );
+    } catch (const std::exception &e) {
+        _env->ThrowNew(
+                _env->FindClass(
+                        "com/simplito/java/privmx_endpoint/model/exceptions/NativeException"),
+                e.what()
+        );
+    } catch (...) {
+        _env->ThrowNew(
+                _env->FindClass(
+                        "com/simplito/java/privmx_endpoint/model/exceptions/NativeException"),
+                "Unknown exception"
+        );
+    }
+}
--- privmx-endpoint/src/main/cpp/utils.hpp
+++ privmx-endpoint/src/main/cpp/utils.hpp
@@ -17,40 +17,57 @@
 
 #include <privmx/endpoint/core/Exception.hpp>
 #include "privmx/endpoint/core/Exception.hpp"
+#include "exceptions.h"
 
-class JniContextUtils
-{
+class JniContextUtils {
 public:
-    class Object
-    {
+    class Object {
     public:
-        Object(JniContextUtils& env, jobject obj);
+        Object(JniContextUtils &env, jobject obj);
+
         ~Object();
-        std::string getFieldAsString(const std::string& name);
+
+        std::string getFieldAsString(const std::string &name);
+
         jlong getLongValue();
+
         jboolean getBooleanValue();
 
     private:
         jobject _obj;
         jclass _objCls;
-        JniContextUtils& _env;
+        JniContextUtils &_env;
     };
 
-    JniContextUtils(JNIEnv* env) : _env(env) {}
-    JNIEnv* operator->() { return _env; }
+    JniContextUtils(JNIEnv *env) : _env(env) {}
+
+    JNIEnv *operator->() { return _env; }
+
     std::string jString2string(jstring str);
+
     std::string jByteArray2String(jbyteArray arr);
+
     jobjectArray jObject2jArray(jobject obj);
+
     Object getObject(jobject obj);
+
     jthrowable coreException2jthrowable(privmx::endpoint::core::Exception exception_c);
+
     jobject long2jLong(long long value);
+
     jobject bool2jBoolean(bool value);
+
     jobject int2jInteger(int value);
+
     bool nullCheck(void *value, std::string value_name);
 
+    template<typename T>
+    void callResultEndpointApi(T *result, const std::function<T()> &fun);
+
+    void callVoidEndpointApi(const std::function<void()> &fun);
 
 private:
-    JNIEnv* _env;
+    JNIEnv *_env;
 };
 
 #endif //PRIVMX_PRIVMXPOCKETLIB_UTILS_HPP
