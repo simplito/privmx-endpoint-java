819575bd2a0d702548dae35ab4e0011af1694f7f
Doominika <dsluzynska@simplito.com>
2025-04-30 19:30:46 +0200

    feat: add BridgeIdentity class

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/BridgeIdentity.java
@@ -0,0 +1,43 @@
+package com.simplito.java.privmx_endpoint.model;
+
+/**
+ * Data send by client to all data informing where was it send
+ */
+public class BridgeIdentity {
+    /**
+     * Bridge URL.
+     */
+    public String url;
+
+    /**
+     * Bridge public Key.
+     */
+    public String pubKey;
+
+    /**
+     * Bridge instance Id given by PKI.
+     */
+    public String instanceId;
+
+    /**
+     * Creates instance of {@code PKIVerificationOptions}.
+     *
+     * @param url Bridge URL
+     */
+    public BridgeIdentity(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Creates instance of {@code PKIVerificationOptions}.
+     *
+     * @param url        Bridge URL
+     * @param pubKey     Bridge public Key
+     * @param instanceId Bridge instance Id given by PKI
+     */
+    public BridgeIdentity(String url, String pubKey, String instanceId) {
+        this.url = url;
+        this.pubKey = pubKey;
+        this.instanceId = instanceId;
+    }
+}
