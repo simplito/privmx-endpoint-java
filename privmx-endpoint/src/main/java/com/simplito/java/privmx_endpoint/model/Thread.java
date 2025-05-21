d241eb14c58d6491c691196dfff61875513f3f56
Doominika <dsluzynska@simplito.com>
2025-05-21 15:40:31 +0200

    feat: add new constructor to Thread

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/Thread.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/Thread.java
@@ -97,6 +97,66 @@ public class Thread {
      */
     public Long statusCode;
 
+    /**
+     * Version of the Thread data structure and how it is encoded/encrypted.
+     */
+    public Long schemaVersion;
+
+    /**
+     * Creates instance of {@code Thread}.
+     *
+     * @param contextId            ID of the Context.
+     * @param threadId             ID of the Thread.
+     * @param createDate           Thread creation timestamp.
+     * @param creator              ID of the user who created the Thread.
+     * @param lastModificationDate Thread last modification timestamp.
+     * @param lastModifier         ID of the user who last modified the Thread.
+     * @param users                List of users (their IDs) with access to the Thread.
+     * @param managers             List of users (their IDs) with management rights.
+     * @param version              Version number (changes on updates).
+     * @param lastMsgDate          Timestamp of the last posted message.
+     * @param publicMeta           Total number of messages in the Thread.
+     * @param privateMeta          Thread's public metadata.
+     * @param policy               Thread's policies.
+     * @param messagesCount        Thread's private metadata.
+     * @param statusCode           Status code of retrieval and decryption of the {@code Thread}.
+     * @param schemaVersion        Version of the Thread data structure and how it is encoded/encrypted.
+     */
+    public Thread(
+            String contextId,
+            String threadId,
+            Long createDate,
+            String creator,
+            Long lastModificationDate,
+            String lastModifier,
+            List<String> users,
+            List<String> managers,
+            Long version,
+            Long lastMsgDate,
+            byte[] publicMeta,
+            byte[] privateMeta,
+            ContainerPolicy policy,
+            Long messagesCount,
+            Long statusCode,
+            Long schemaVersion
+    ) {
+        this.contextId = contextId;
+        this.threadId = threadId;
+        this.createDate = createDate;
+        this.creator = creator;
+        this.lastModificationDate = lastModificationDate;
+        this.lastModifier = lastModifier;
+        this.users = users;
+        this.managers = managers;
+        this.version = version;
+        this.lastMsgDate = lastMsgDate;
+        this.publicMeta = publicMeta;
+        this.privateMeta = privateMeta;
+        this.policy = policy;
+        this.messagesCount = messagesCount;
+        this.statusCode = statusCode;
+        this.schemaVersion = schemaVersion;
+    }
 
     /**
      * Creates instance of {@code Thread}.
@@ -134,21 +194,7 @@ public class Thread {
             Long messagesCount,
             Long statusCode
     ) {
-        this.contextId = contextId;
-        this.threadId = threadId;
-        this.createDate = createDate;
-        this.creator = creator;
-        this.lastModificationDate = lastModificationDate;
-        this.lastModifier = lastModifier;
-        this.users = users;
-        this.managers = managers;
-        this.version = version;
-        this.lastMsgDate = lastMsgDate;
-        this.publicMeta = publicMeta;
-        this.privateMeta = privateMeta;
-        this.policy = policy;
-        this.messagesCount = messagesCount;
-        this.statusCode = statusCode;
+        this(contextId, threadId, createDate, creator, lastModificationDate, lastModifier, users, managers, version, lastMsgDate, publicMeta, privateMeta, null, messagesCount, statusCode, null);
     }
 
     /**
