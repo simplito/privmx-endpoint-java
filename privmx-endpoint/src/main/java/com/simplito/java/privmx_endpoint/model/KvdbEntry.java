fb01f72ae4f7cc41ad982ec466d7173576e10752
Doominika <dsluzynska@simplito.com>
2025-05-30 14:41:49 +0200

    refactor: update KvdbEntry class

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/KvdbEntry.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/KvdbEntry.java
@@ -1,45 +1,71 @@
 package com.simplito.java.privmx_endpoint.model;
 
 /**
- * Holds all available information about a Item.
+ * Holds all available information about a Entry.
+ *
+ * @category kvdb
  */
 public class KvdbEntry {
     /**
-     * Item information created by server
+     * Entry information created by server
      */
     ServerKvdbEntryInfo info;
 
     /**
-     * Item public metadata
+     * Entry public metadata
      */
     byte[] publicMeta;
 
     /**
-     * Item private metadata
+     * Entry private metadata
      */
     byte[] privateMeta;
 
     /**
-     * Item data
+     * Entry data
      */
     byte[] data;
 
     /**
-     * public key of an author of the message
+     * Public key of an author of the entry
      */
     String authorPubKey;
 
     /**
-     * status code of retrieval and decryption of the Kvdb
+     * Version number (changes on every on existing item)
+     */
+    long version;
+
+    /**
+     * Retrieval and decryption status code
      */
     long statusCode;
 
-    public KvdbEntry(ServerKvdbEntryInfo info, byte[] publicMeta, byte[] privateMeta, byte[] data, String authorPubKey, long statusCode) {
+    /**
+     * Version of the Entry data structure and how it is encoded/encrypted
+     */
+    long schemaVersion;
+
+    /**
+     * Creates instance of {@code Kvdb}.
+     *
+     * @param info          Entry information created by server
+     * @param publicMeta    Entry public metadata
+     * @param privateMeta   Entry private metadata
+     * @param data          Entry data
+     * @param authorPubKey  Public key of an author of the entry
+     * @param version       Version number (changes on every on existing item)
+     * @param statusCode    Retrieval and decryption status code
+     * @param schemaVersion Version of the Entry data structure and how it is encoded/encrypted
+     */
+    public KvdbEntry(ServerKvdbEntryInfo info, byte[] publicMeta, byte[] privateMeta, byte[] data, String authorPubKey, long version, long statusCode, long schemaVersion) {
         this.info = info;
         this.publicMeta = publicMeta;
         this.privateMeta = privateMeta;
         this.data = data;
         this.authorPubKey = authorPubKey;
+        this.version = version;
         this.statusCode = statusCode;
+        this.schemaVersion = schemaVersion;
     }
-};
+}
