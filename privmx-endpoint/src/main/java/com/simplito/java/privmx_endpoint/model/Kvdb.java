1ac9467efb89c036cdbefbb6ce20ec676ad63073
Doominika <dsluzynska@simplito.com>
2025-05-30 14:31:31 +0200

    refactor: update Kvdb class

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/Kvdb.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/Kvdb.java
@@ -2,6 +2,11 @@ package com.simplito.java.privmx_endpoint.model;
 
 import java.util.List;
 
+/**
+ * Holds all available information about a Kvdb.
+ *
+ * @category kvdb
+ */
 public class Kvdb {
 
     /**
@@ -35,17 +40,17 @@ public class Kvdb {
     String lastModifier;
 
     /**
-     * list of users (their IDs) with access to the Kvdb
+     * List of users (their IDs) with access to the Kvdb
      */
     List<String> users;
 
     /**
-     * list of users (their IDs) with management rights
+     * List of users (their IDs) with management rights
      */
     List<String> managers;
 
     /**
-     * version number (changes on updates)
+     * Version number (changes on updates)
      */
     long version;
 
@@ -60,14 +65,14 @@ public class Kvdb {
     byte[] privateMeta;
 
     /**
-     * total number of items in the Kvdb
+     * Total number of entries in the Kvdb
      */
-    long items;
+    long entries;
 
     /**
-     * timestamp of last new item
+     * Timestamp of the last added entry
      */
-    long lastItemDate;
+    long lastEntryDate;
 
     /**
      * Kvdb's policies
@@ -75,11 +80,36 @@ public class Kvdb {
     ContainerPolicy policy;
 
     /**
-     * status code of retrieval and decryption of the Kvdb
+     * Retrieval and decryption status code
      */
     long statusCode;
 
-    public Kvdb(String contextId, String kvdbId, long createDate, String creator, long lastModificationDate, String lastModifier, List<String> users, List<String> managers, long version, byte[] publicMeta, byte[] privateMeta, long items, long lastItemDate, ContainerPolicy policy, long statusCode) {
+    /**
+     * Version of the Kvdb data structure and how it is encoded/encrypted
+     */
+    long schemaVersion;
+
+    /**
+     * Creates instance of {@code Kvdb}.
+     *
+     * @param contextId            ID of the Context
+     * @param kvdbId               ID of the Kvdb
+     * @param createDate           Kvdb creation timestamp
+     * @param creator              ID of user who created the Kvdb
+     * @param lastModificationDate Kvdb last modification timestamp
+     * @param lastModifier         ID of the user who last modified the Kvdb
+     * @param users                List of users (their IDs) with access to the Kvdb
+     * @param managers             List of users (their IDs) with management rights
+     * @param version              Version number (changes on updates)
+     * @param publicMeta           Kvdb's public metadata
+     * @param privateMeta          Kvdb's private metadata
+     * @param entries              Total number of entries in the Kvdb
+     * @param lastEntryDate        Timestamp of the last added entry
+     * @param policy               Kvdb's policies
+     * @param statusCode           Retrieval and decryption status code
+     * @param schemaVersion        Version of the Kvdb data structure and how it is encoded/encrypted
+     */
+    public Kvdb(String contextId, String kvdbId, long createDate, String creator, long lastModificationDate, String lastModifier, List<String> users, List<String> managers, long version, byte[] publicMeta, byte[] privateMeta, long entries, long lastEntryDate, ContainerPolicy policy, long statusCode, long schemaVersion) {
         this.contextId = contextId;
         this.kvdbId = kvdbId;
         this.createDate = createDate;
@@ -91,9 +121,10 @@ public class Kvdb {
         this.version = version;
         this.publicMeta = publicMeta;
         this.privateMeta = privateMeta;
-        this.items = items;
-        this.lastItemDate = lastItemDate;
+        this.entries = entries;
+        this.lastEntryDate = lastEntryDate;
         this.policy = policy;
         this.statusCode = statusCode;
+        this.schemaVersion = schemaVersion;
     }
 }
