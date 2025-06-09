ddf3b61429bfe29f7d95e5739019c3ea4145b9a5
Dawid Jenczewski <djenczewski@simplito.com>
2025-06-09 12:28:14 +0200

    docs(privmx-endpoint): update `extKey` param description in constructor

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/BIP39.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/BIP39.java
@@ -1,3 +1,14 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.simplito.java.privmx_endpoint.model;
 
 import com.simplito.java.privmx_endpoint.modules.crypto.ExtKey;
@@ -25,7 +36,7 @@ public class BIP39 {
      * Creates instance of {@code BIP39}.
      *
      * @param mnemonic BIP-39 mnemonic
-     * @param extKey   BIP-39 mnemonic
+     * @param extKey   BIP-39 Ecc Key
      * @param entropy  BIP-39 entropy
      */
     public BIP39(String mnemonic, ExtKey extKey, byte[] entropy) {
