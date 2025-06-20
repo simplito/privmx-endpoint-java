29303bb5c3ce170883972c3ee0c2eaf3756c8d7e
Dawid Jenczewski <djenczewski@simplito.com>
2025-06-20 11:18:57 +0200

    chore: add copyright to new created files

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/modules/crypto/ExtKey.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/modules/crypto/ExtKey.java
@@ -1,3 +1,14 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.simplito.java.privmx_endpoint.modules.crypto;
 
 import com.simplito.java.privmx_endpoint.model.exceptions.NativeException;
@@ -122,7 +133,7 @@ public class ExtKey implements AutoCloseable {
      * @return returns true if ExtKey is private
      */
     public native boolean isPrivate() throws IllegalStateException, PrivmxException, NativeException;
-    
+
     private native void deinit() throws IllegalStateException, PrivmxException, NativeException;
 
     @Override
