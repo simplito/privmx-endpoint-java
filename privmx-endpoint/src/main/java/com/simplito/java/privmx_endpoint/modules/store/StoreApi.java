cc0c5ea6aa2e9eda6ab06d8ead04195dce9999cd
Doominika <dsluzynska@simplito.com>
2025-06-23 11:56:39 +0200

    feat: add new sortBy parameter to listStores and listFiles in StoreApi

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/modules/store/StoreApi.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/modules/store/StoreApi.java
@@ -186,7 +186,7 @@ public class StoreApi implements AutoCloseable {
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
     public PagingList<Store> listStores(String contextId, long skip, long limit, String sortOrder) throws PrivmxException, NativeException, IllegalStateException {
-        return listStores(contextId, skip, limit, sortOrder, null, null);
+        return listStores(contextId, skip, limit, sortOrder, null, null, null);
     }
 
     /**
@@ -203,7 +203,7 @@ public class StoreApi implements AutoCloseable {
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
     public PagingList<Store> listStores(String contextId, long skip, long limit, String sortOrder, String lastId) throws PrivmxException, NativeException, IllegalStateException {
-        return listStores(contextId, skip, limit, sortOrder, lastId, null);
+        return listStores(contextId, skip, limit, sortOrder, lastId, null, null);
     }
 
     /**
@@ -220,7 +220,26 @@ public class StoreApi implements AutoCloseable {
      * @throws PrivmxException       thrown when method encounters an exception.
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
-    public native PagingList<Store> listStores(String contextId, long skip, long limit, String sortOrder, String lastId, String queryAsJson) throws PrivmxException, NativeException, IllegalStateException;
+    public PagingList<Store> listStores(String contextId, long skip, long limit, String sortOrder, String lastId, String queryAsJson) throws PrivmxException, NativeException, IllegalStateException {
+        return listStores(contextId, skip, limit, sortOrder, lastId, queryAsJson, null);
+    }
+
+    /**
+     * Gets a list of Stores in given Context.
+     *
+     * @param contextId   ID of the Context to get the Stores from
+     * @param skip        skip number of elements to skip from result
+     * @param limit       limit of elements to return for query
+     * @param sortOrder   order of elements in result ("asc" for ascending, "desc" for descending)
+     * @param lastId      ID of the element from which query results should start
+     * @param queryAsJson stringified JSON object with a custom field to filter result
+     * @param sortBy      field by elements are sorted in result
+     * @return list of Stores
+     * @throws IllegalStateException thrown when instance is closed.
+     * @throws PrivmxException       thrown when method encounters an exception.
+     * @throws NativeException       thrown when method encounters an unknown exception.
+     */
+    public native PagingList<Store> listStores(String contextId, long skip, long limit, String sortOrder, String lastId, String queryAsJson, String sortBy) throws PrivmxException, NativeException, IllegalStateException;
 
     /**
      * Deletes a Store by given Store ID.
@@ -327,7 +346,7 @@ public class StoreApi implements AutoCloseable {
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
     public PagingList<File> listFiles(String storeId, long skip, long limit, String sortOrder) throws PrivmxException, NativeException, IllegalStateException {
-        return listFiles(storeId, skip, limit, sortOrder, null, null);
+        return listFiles(storeId, skip, limit, sortOrder, null, null, null);
     }
 
     /**
@@ -344,7 +363,7 @@ public class StoreApi implements AutoCloseable {
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
     public PagingList<File> listFiles(String storeId, long skip, long limit, String sortOrder, String lastId) throws PrivmxException, NativeException, IllegalStateException {
-        return listFiles(storeId, skip, limit, sortOrder, lastId, null);
+        return listFiles(storeId, skip, limit, sortOrder, lastId, null, null);
     }
 
     /**
@@ -361,7 +380,26 @@ public class StoreApi implements AutoCloseable {
      * @throws PrivmxException       thrown when method encounters an exception.
      * @throws NativeException       thrown when method encounters an unknown exception.
      */
-    public native PagingList<File> listFiles(String storeId, long skip, long limit, String sortOrder, String lastId, String queryAsJson) throws PrivmxException, NativeException, IllegalStateException;
+    public PagingList<File> listFiles(String storeId, long skip, long limit, String sortOrder, String lastId, String queryAsJson) throws PrivmxException, NativeException, IllegalStateException {
+        return listFiles(storeId, skip, limit, sortOrder, lastId, queryAsJson, null);
+    }
+
+    /**
+     * Gets a list of files in given Store.
+     *
+     * @param storeId     ID of the Store to get files from
+     * @param skip        skip number of elements to skip from result
+     * @param limit       limit of elements to return for query
+     * @param sortOrder   order of elements in result ("asc" for ascending, "desc" for descending)
+     * @param lastId      ID of the element from which query results should start
+     * @param queryAsJson stringified JSON object with a custom field to filter result
+     * @param sortBy      field by elements are sorted in result
+     * @return list of files
+     * @throws IllegalStateException thrown when instance is closed.
+     * @throws PrivmxException       thrown when method encounters an exception.
+     * @throws NativeException       thrown when method encounters an unknown exception.
+     */
+    public native PagingList<File> listFiles(String storeId, long skip, long limit, String sortOrder, String lastId, String queryAsJson, String sortBy) throws PrivmxException, NativeException, IllegalStateException;
 
     /**
      * Opens a file to read.
