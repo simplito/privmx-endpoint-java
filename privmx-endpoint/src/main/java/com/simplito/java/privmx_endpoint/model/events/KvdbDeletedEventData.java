42c5bc027aa58b99d93c3b9649026e81b9400a2b
Doominika <dsluzynska@simplito.com>
2025-06-25 12:42:00 +0200

    docs: add tags to KvdbDeletedEventData class

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/KvdbDeletedEventData.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/KvdbDeletedEventData.java
@@ -2,6 +2,9 @@ package com.simplito.java.privmx_endpoint.model.events;
 
 /**
  * Holds information of `KvdbDeletedEvent`.
+ *
+ * @category core
+ * @group Events
  */
 public class KvdbDeletedEventData {
 
