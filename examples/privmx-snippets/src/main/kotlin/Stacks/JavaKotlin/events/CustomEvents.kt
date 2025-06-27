c70ec98b3a47f7a5ac6f8291a25df48f98159f31
Doominika <101605588+Doominika@users.noreply.github.com>
2025-06-27 12:04:49 +0200

    Prepare snippets for 2.3.0 (#57)
    * feat: add setUserVerifier snippet
    
    * feat: add snippets for custom fields and queries
    
    * feat: add snippets for custom events
    
    * feat: add initializing eventApi snippet
    
    * feat: add eventApi to setting connection snippet in PrivMXEndpointJava
    
    * feat: selected one variant of creating json in CustomFieldsAndQueries
    
    * chore: add comment in setUserVerifier method
    
    * refactor: put each snippet with custom query into a separate function
    
    * chore: move parameters to new lines in custom events snippets
    
    * refactor: update snippets for custom queries
    
    * fix: correct functions names

---
+++ examples/privmx-snippets/src/main/kotlin/Stacks/JavaKotlin/events/CustomEvents.kt
@@ -0,0 +1,44 @@
+package Stacks.JavaKotlin.events
+
+import Stacks.JavaKotlin.endpointSession
+import Stacks.JavaKotlin.user1Id
+import Stacks.JavaKotlin.user1PublicKey
+import Stacks.JavaKotlin.user2Id
+import Stacks.JavaKotlin.user2PublicKey
+import com.simplito.java.privmx_endpoint.model.UserWithPubKey
+import com.simplito.java.privmx_endpoint.modules.event.EventApi
+import com.simplito.java.privmx_endpoint_extra.events.EventType
+
+lateinit var eventApi: EventApi
+
+fun setEventApi() {
+    val eventApi = endpointSession.eventApi
+}
+
+fun emittingCustomEvents() {
+    val contextId = "CONTEXT_ID"
+    val channelName = "CHANNEL_NAME"
+
+    val users: List<UserWithPubKey> = listOf(
+        UserWithPubKey(user1Id, user1PublicKey),
+        UserWithPubKey(user2Id, user2PublicKey)
+    )
+    val eventData = "Custom Event Data"
+
+    endpointSession.eventApi.emitEvent(
+        contextId, users, channelName, eventData.encodeToByteArray()
+    )
+}
+
+fun handlingCustomEvents() {
+    val callbacksId = "CALLBACKS_ID"
+    val contextId = "CONTEXT_ID"
+    val channelName = "CHANNEL_NAME"
+
+    endpointSession.registerCallback(
+        callbacksId,
+        EventType.ContextCustomEvent(contextId, channelName)
+    ) { customEventData ->
+        // Some actions when custom event arrives
+    }
+}
