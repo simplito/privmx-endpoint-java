9efcb483029a43758f5b431582e0cc3ccab43f8b
Dawid Jenczewski <djenczewski@simplito.com>
2025-07-29 15:48:55 +0200

    feat: add EventApi.h with getEventApi method declaration

---
+++ privmx-endpoint/src/main/cpp/modules/EventApi.h
@@ -0,0 +1,15 @@
+//
+// Created by Dawid Jenczewski on 29/07/2025.
+//
+
+#include <jni.h>
+#include <privmx/endpoint/event/EventApi.hpp>
+#include "../utils.hpp"
+
+#ifndef PRIVMXENDPOINT_EVENTAPI_H
+#define PRIVMXENDPOINT_EVENTAPI_H
+
+#endif //PRIVMXENDPOINT_EVENTAPI_H
+
+
+privmx::endpoint::event::EventApi *getEventApi(JniContextUtils &ctx, jobject eventApiInstance);
