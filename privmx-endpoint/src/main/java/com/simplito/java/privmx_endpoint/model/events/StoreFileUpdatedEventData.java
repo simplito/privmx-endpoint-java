e874bc25b7b529187e41d4e35a4c1da72e0204a6
Doominika <dsluzynska@simplito.com>
2025-08-04 14:06:44 +0200

    feat: add StoreFileUpdatedEventData class

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/StoreFileUpdatedEventData.java
@@ -0,0 +1,35 @@
+package com.simplito.java.privmx_endpoint.model.events;
+
+import com.simplito.java.privmx_endpoint.model.File;
+import com.simplito.java.privmx_endpoint.model.FileChange;
+
+import java.util.List;
+
+/**
+ * Holds information about the file change.
+ *
+ * @category core
+ * @group Events
+ */
+public class StoreFileUpdatedEventData {
+    /**
+     * File meta
+     */
+    public final File file;
+
+    /**
+     * List of file changes
+     */
+    public final List<FileChange> changes;
+
+    /**
+     * Creates instance of {@code StoreFileUpdatedEventData}.
+     *
+     * @param file    File meta
+     * @param changes List of file changes
+     */
+    public StoreFileUpdatedEventData(File file, List<FileChange> changes) {
+        this.file = file;
+        this.changes = changes;
+    }
+}
