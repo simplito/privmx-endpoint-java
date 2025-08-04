28783f02f51441b02dca8dd3edef43cf5e456a09
Doominika <dsluzynska@simplito.com>
2025-08-04 15:37:53 +0200

    refactor: create EventType interface and use it in EventType classes

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventTypes/InboxEventType.java
@@ -0,0 +1,9 @@
+package com.simplito.java.privmx_endpoint.model.events.eventTypes;
+
+public enum InboxEventType implements EventType {
+    INBOX_CREATE,
+    INBOX_UPDATE,
+    INBOX_DELETE,
+    ENTRY_CREATE,
+    ENTRY_DELETE
+}
--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventTypes/KvdbEventType.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventTypes/KvdbEventType.java
@@ -1,6 +1,6 @@
-package com.simplito.java.privmx_endpoint.model;
+package com.simplito.java.privmx_endpoint.model.events.eventTypes;
 
-public enum KvdbEventType {
+public enum KvdbEventType implements EventType {
     KVDB_CREATE,
     KVDB_UPDATE,
     KVDB_DELETE,
