b2dbf080482ce0ad30093508a8a7c913c7af4a4a
Doominika <dsluzynska@simplito.com>
2025-08-06 13:34:57 +0200

    feat: implement in jni parser to java for new StoreFileUpdatedEventData class

--- privmx-endpoint/src/main/cpp/model_native_initializers.h
+++ privmx-endpoint/src/main/cpp/model_native_initializers.h
@@ -109,7 +109,8 @@ namespace privmx {
         //Event
         jobject storeDeletedEventData2Java(JniContextUtils &ctx,
                                            privmx::endpoint::store::StoreDeletedEventData storeDeletedEventData_c);
-
+        jobject storeFileUpdatedEventData2Java(JniContextUtils &ctx,
+                                               privmx::endpoint::store::StoreFileUpdatedEventData storeFileUpdatedEventData_c);
         jobject storeFileDeletedEventData2Java(JniContextUtils &ctx,
                                                privmx::endpoint::store::StoreFileDeletedEventData storeFileDeletedEventData_c);
 
