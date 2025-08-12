0d4092d8f1fd4f0d9d8037b0fa9732bc8eb68f54
Doominika <dsluzynska@simplito.com>
2025-08-12 13:58:16 +0200

    feat: add snippets related to inboxes

---
+++ examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/InboxEntries.java
@@ -0,0 +1,128 @@
+package Tools.Inboxes.UsingInboxes;
+
+import com.simplito.java.privmx_endpoint.model.File;
+import com.simplito.java.privmx_endpoint.model.InboxEntry;
+import com.simplito.java.privmx_endpoint.model.PagingList;
+import com.simplito.java.privmx_endpoint_extra.inboxFileStream.InboxFileStreamWriter;
+import com.simplito.java.privmx_endpoint_extra.model.SortOrder;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class InboxEntries extends WorkingWithInboxes {
+    void sendingInboxEntryBasic() {
+        // 1. Preparing Entry
+        String inboxID = "INBOX_ID";
+
+        String entryDataAnswer = "USER_PROVIDED_TEXT";
+        long entryDataVersion = 1L;
+        String entryDataType = "TEXT_ANSWER";
+
+        byte[] entryData = """
+                 {
+                    "content": {
+                        "answer": "%s
+                    },
+                    "version": %d,
+                    "type": %s
+                 }
+                """.formatted(entryDataAnswer, entryDataVersion, entryDataType).getBytes();
+
+        long entryHandle = endpointSession.inboxApi.prepareEntry(
+                inboxID,
+                entryData,
+                List.of()
+        );
+
+        // 2. Sending Entry
+        endpointSession.inboxApi.sendEntry(entryHandle);
+    }
+
+    void attachingFiles(String fileID) throws IOException {
+        String inboxID = "INBOX_ID";
+        byte[] privateMeta = "My private data".getBytes(StandardCharsets.UTF_8);
+        byte[] publicMeta = "My public data".getBytes(StandardCharsets.UTF_8);
+
+        String fileName = "FILE_NAME";
+        String fileType = "FILE_TYPE";
+        byte[] fileContent = "FILE_CONTENT".getBytes();
+        long fileSize = fileContent.length;
+
+        byte[] entryData = """
+                {
+                    "name": "%s",
+                    "mimetype": "%s"
+                }
+                """.formatted(fileName, fileType).getBytes();
+
+        InboxFileStreamWriter inboxFileStreamWriter = InboxFileStreamWriter.createFile(
+                endpointSession.inboxApi,
+                publicMeta,
+                privateMeta,
+                fileSize
+        );
+
+        long fileHandle = inboxFileStreamWriter.getFileHandle();
+
+        // 1. Preparing Entry & sending File Contents
+        long inboxHandle = endpointSession.inboxApi.prepareEntry(
+                inboxID,
+                entryData,
+                List.of(fileHandle)
+        );
+
+        // 2. Sending File Contents
+        inboxFileStreamWriter.write(
+                inboxHandle,
+                fileContent
+        );
+
+        // 3. Sending Entry
+        endpointSession.inboxApi.sendEntry(inboxHandle);
+
+    }
+
+    void fetchingEntriesBasic() {
+        String inboxID = "INBOX_ID";
+        long startIndex = 0L;
+        long pageSize = 100L;
+
+        PagingList<InboxEntry> entriesPagingList = endpointSession.inboxApi.listEntries(
+                inboxID,
+                startIndex,
+                pageSize,
+                SortOrder.DESC
+        );
+    }
+
+    void fetchingEntriesWithFiles() {
+        String inboxID = "INBOX_ID";
+        long startIndex = 0L;
+        long pageSize = 1L;
+
+        // getting last entry
+        PagingList<InboxEntry> entriesPagingList = endpointSession.inboxApi.listEntries(
+                inboxID,
+                startIndex,
+                pageSize,
+                SortOrder.DESC
+        );
+
+        InboxEntry entry = entriesPagingList.readItems.getFirst();
+        File entryFiles = entry.files.getFirst();
+
+        // decoded privateMeta
+        String privateMeta = new String(entry.files.getFirst().privateMeta);
+        JSONObject privateMetaJson = new JSONObject(privateMeta);
+        String fileName = privateMetaJson.getString("name");
+        String fileType = privateMetaJson.getString("mimetype");
+
+        // decoded data
+        String entryData = new String(entry.data);
+        JSONObject entryDataJson = new JSONObject(entryData);
+        String answer = entryDataJson.getJSONObject("content").getString("answer");
+    }
+}
---
+++ examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/InboxUpdates.java
@@ -0,0 +1,42 @@
+package Tools.Inboxes.UsingInboxes;
+
+import com.simplito.java.privmx_endpoint_extra.events.EventType;
+import com.simplito.java.privmx_endpoint_extra.lib.PrivmxEndpointContainer;
+
+// todo - Inbox Updates - why this name ?!
+public class InboxUpdates extends WorkingWithInboxes {
+    void handlingInboxEvents() {
+        PrivmxEndpointContainer container = new PrivmxEndpointContainer();
+        Long connectionID = 0L;
+        String InboxCallbackID = "INBOX_CALLBACK_ID";
+        String EntryCallbackID = "ENTRY_CALLBACK_ID";
+        String inboxID = "INBOX_ID";
+
+        // Starting the Event Loop
+        container.startListening();
+
+        // Handling Inbox Events
+        container.getEndpoint(connectionID).registerCallback(
+                InboxCallbackID,
+                EventType.InboxUpdatedEvent,
+                updatedInbox -> {
+                    System.out.println(updatedInbox.lastModifier);
+                }
+        );
+
+        // Handling Inbox Entry Events
+        container.getEndpoint(connectionID).registerCallback(
+                EntryCallbackID,
+                EventType.InboxEntryCreatedEvent(inboxID),
+                newEntry -> {
+                    System.out.println(newEntry.inboxId);
+                }
+        );
+
+        // Finish handling events
+        container.getEndpoint(connectionID).unregisterCallbacks(InboxCallbackID);
+        container.getEndpoint(connectionID).unregisterCallbacks(EntryCallbackID);
+
+        container.getEndpoint(connectionID).unregisterAll();
+    }
+}
