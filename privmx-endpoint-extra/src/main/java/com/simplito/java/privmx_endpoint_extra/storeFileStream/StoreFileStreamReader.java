8d165a4eedbb3d5359a3b3deb7689316b259c3d4
Doominika <dsluzynska@simplito.com>
2025-08-25 13:35:52 +0200

    feat(privmx-endpoint-extra): add syncFile method to StoreFileStreamReader class

--- privmx-endpoint-extra/src/main/java/com/simplito/java/privmx_endpoint_extra/storeFileStream/StoreFileStreamReader.java
+++ privmx-endpoint-extra/src/main/java/com/simplito/java/privmx_endpoint_extra/storeFileStream/StoreFileStreamReader.java
@@ -127,6 +127,25 @@ public class StoreFileStreamReader extends StoreFileStream {
     }
 
     /**
+     * Synchronizes file data with newest data on server
+     *
+     * @param api    reference to Store API
+     * @return Instance ready to read from the Store file
+     * @throws IllegalStateException when {@code storeApi} is not initialized or there's no connection
+     * @throws PrivmxException       if there is an error while opening Store file
+     * @throws NativeException       if there is an unknown error while opening Store file
+     */
+    public StoreFileStreamReader syncFile(
+            StoreApi api
+    ) throws IllegalStateException, PrivmxException, NativeException {
+        api.syncFile(handle);
+        return new StoreFileStreamReader(
+                handle,
+                api
+        );
+    }
+
+    /**
      * Moves read cursor.
      *
      * @param position new cursor position
