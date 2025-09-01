67bfc28addbab06988da78633145ff697c6704d3
Doominika <dsluzynska@simplito.com>
2025-09-01 12:04:15 +0200

    feat: update snippet for handling inbox events

--- examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/InboxUpdates.java
+++ examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/InboxUpdates.java
@@ -1,21 +1,18 @@
 package Tools.Inboxes.UsingInboxes;
 
 import com.simplito.java.privmx_endpoint_extra.events.EventType;
-import com.simplito.java.privmx_endpoint_extra.lib.PrivmxEndpointContainer;
 
 public class InboxUpdates extends WorkingWithInboxes {
     void handlingInboxEvents() {
-        PrivmxEndpointContainer container = new PrivmxEndpointContainer();
-        Long connectionID = 0L;
         String InboxCallbackID = "INBOX_CALLBACK_ID";
         String EntryCallbackID = "ENTRY_CALLBACK_ID";
         String inboxID = "INBOX_ID";
 
         // Starting the Event Loop
-        container.startListening();
+        endpointContainer.startListening();
 
         // Handling Inbox Events
-        container.getEndpoint(connectionID).registerCallback(
+        endpointSession.registerCallback(
                 InboxCallbackID,
                 EventType.InboxUpdatedEvent,
                 updatedInbox -> {
@@ -24,7 +21,7 @@ public class InboxUpdates extends WorkingWithInboxes {
         );
 
         // Handling Inbox Entry Events
-        container.getEndpoint(connectionID).registerCallback(
+        endpointSession.registerCallback(
                 EntryCallbackID,
                 EventType.InboxEntryCreatedEvent(inboxID),
                 newEntry -> {
@@ -33,9 +30,9 @@ public class InboxUpdates extends WorkingWithInboxes {
         );
 
         // Finish handling events
-        container.getEndpoint(connectionID).unregisterCallbacks(InboxCallbackID);
-        container.getEndpoint(connectionID).unregisterCallbacks(EntryCallbackID);
+        endpointSession.unregisterCallbacks(InboxCallbackID);
+        endpointSession.unregisterCallbacks(EntryCallbackID);
 
-        container.getEndpoint(connectionID).unregisterAll();
+        endpointSession.unregisterAll();
     }
 }
