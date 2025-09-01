a0731ac3bf2d7868c555e68d86341c65c172f1f8
Doominika <dsluzynska@simplito.com>
2025-09-01 11:54:54 +0200

    feat: update snippet for handling kvdb events

--- examples/privmx-snippets/src/main/java/Tools/Kvdbs/UsingKvdbs/ListeningForChanges.java
+++ examples/privmx-snippets/src/main/java/Tools/Kvdbs/UsingKvdbs/ListeningForChanges.java
@@ -5,17 +5,15 @@ import com.simplito.java.privmx_endpoint_extra.lib.PrivmxEndpointContainer;
 
 public class ListeningForChanges extends ManagingKvdbs {
     void handlingKvdbEvents() {
-        PrivmxEndpointContainer container = new PrivmxEndpointContainer();
-        Long connectionID = 0L;
         String KvdbCallbackID = "KVDB_CALLBACK_ID";
         String EntrycallbackID = "ENTRY_CALLBACK_ID";
         String kvdbID = "KVDB_ID";
 
         // Starting the Event Loop
-        container.startListening();
+        endpointContainer.startListening();
 
         // Handling KVDB Events
-        container.getEndpoint(connectionID).registerCallback(
+        endpointSession.registerCallback(
                 KvdbCallbackID,
                 EventType.KvdbStatsEvent,
                 kvdbStats -> {
@@ -24,7 +22,7 @@ public class ListeningForChanges extends ManagingKvdbs {
         );
 
         // Handling KVDB Entry Events
-        container.getEndpoint(connectionID).registerCallback(
+        endpointSession.registerCallback(
                 EntrycallbackID,
                 EventType.KvdbNewEntryEvent(kvdbID),
                 newEntry -> {
@@ -33,7 +31,7 @@ public class ListeningForChanges extends ManagingKvdbs {
         );
 
         // Finish handling events
-        container.getEndpoint(connectionID).unregisterCallbacks(KvdbCallbackID);
-        container.getEndpoint(connectionID).unregisterCallbacks(EntrycallbackID);
+        endpointSession.unregisterCallbacks(KvdbCallbackID);
+        endpointSession.unregisterCallbacks(EntrycallbackID);
     }
 }
