a792fb6b30118823a1202faf0364c9a016931d92
Doominika <dsluzynska@simplito.com>
2025-09-02 14:49:16 +0200

    feat: add constructor to ContextUserEventData

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/ContextUserEventData.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/ContextUserEventData.java
@@ -4,12 +4,23 @@ import com.simplito.java.privmx_endpoint.model.UserWithPubKey;
 
 public class ContextUserEventData {
     /**
-     * ID of the Context
+     * ID of the Context.
      */
     public String contextId;
 
     /**
-     * User
+     * User.
      */
     public UserWithPubKey user;
+
+    /**
+     * Creates instance of {@code ContextUserEventData}.
+     *
+     * @param contextId ID of the Context
+     * @param user      User
+     */
+    public ContextUserEventData(String contextId, UserWithPubKey user) {
+        this.contextId = contextId;
+        this.user = user;
+    }
 }
