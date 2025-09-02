3cdacdc988f2948a76555b0117442dc641582b14
Doominika <dsluzynska@simplito.com>
2025-09-02 14:50:12 +0200

    feat: add new CollectionItemChange class needed in new event data

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/CollectionItemChange.java
@@ -0,0 +1,27 @@
+package com.simplito.java.privmx_endpoint.model;
+
+/**
+ * Contains information about the changed item in the collection.
+ */
+public class CollectionItemChange {
+    /**
+     * ID of the item.
+     */
+    public String itemId;
+
+    /**
+     * Item change action, which can be "create", "update" or "delete".
+     */
+    public String action;
+
+    /**
+     * Creates instance of {@code CollectionItemChange}.
+     *
+     * @param itemId ID of the item
+     * @param action Item change action, which can be "create", "update" or "delete"
+     */
+    public CollectionItemChange(String itemId, String action) {
+        this.itemId = itemId;
+        this.action = action;
+    }
+}
