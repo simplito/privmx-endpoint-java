f93c52a1d752ced44554ef966a481b823124d239
Doominika <dsluzynska@simplito.com>
2025-09-02 14:50:50 +0200

    feat: add new CollectionChangedEventData event data

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/CollectionChangedEventData.java
@@ -0,0 +1,48 @@
+package com.simplito.java.privmx_endpoint.model.events;
+
+import com.simplito.java.privmx_endpoint.model.CollectionItemChange;
+
+import java.util.List;
+
+/**
+ * Holds data of event that arrives when the collection is changed.
+ *
+ * @category core
+ * @group Events
+ */
+public class CollectionChangedEventData {
+    /**
+     * Type of the module.
+     */
+    public String moduleType;
+
+    /**
+     * ID of the module.
+     */
+    public String moduleId;
+
+    /**
+     * Count of affected items.
+     */
+    public long affectedItemsCount;
+
+    /**
+     * List of item changes.
+     */
+    public List<CollectionItemChange> items;
+
+    /**
+     * Creates instance of {@code CollectionChangedEventData}.
+     *
+     * @param moduleType         Type of the module
+     * @param moduleId           ID of the module
+     * @param affectedItemsCount Count of affected items
+     * @param items              List of item changes
+     */
+    public CollectionChangedEventData(String moduleType, String moduleId, long affectedItemsCount, List<CollectionItemChange> items) {
+        this.moduleType = moduleType;
+        this.moduleId = moduleId;
+        this.affectedItemsCount = affectedItemsCount;
+        this.items = items;
+    }
+}
