9e861a64848e1c12101a687f3e8196e4e1a0419a
Doominika <dsluzynska@simplito.com>
2025-09-02 14:57:02 +0200

    feat: add new UserStatusChange class

---
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/UserStatusChange.java
@@ -0,0 +1,27 @@
+package com.simplito.java.privmx_endpoint.model;
+
+/**
+ * Contains information about the change of user status.
+ */
+public class UserStatusChange {
+    /**
+     * User status change action, which can be "login" or "logout".
+     */
+    public String action;
+
+    /**
+     * Timestamp of the change.
+     */
+    public long timestamp;
+
+    /**
+     * Creates instance of {@code UserStatusChange}
+     *
+     * @param timestamp User status change action, which can be "login" or "logout"
+     * @param action    Timestamp of the change
+     */
+    public UserStatusChange(long timestamp, String action) {
+        this.timestamp = timestamp;
+        this.action = action;
+    }
+}
