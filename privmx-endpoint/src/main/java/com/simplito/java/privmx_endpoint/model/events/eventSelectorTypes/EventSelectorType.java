4e4bdcda6bbd981a01416b62588b2a5b7da9dc27
Dawid Jenczewski <djenczewski@simplito.com>
2025-09-08 11:23:38 +0200

    docs(privmx-endpoint): add category and group tags to EventType and EventSelectorType interfaces

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventSelectorTypes/EventSelectorType.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventSelectorTypes/EventSelectorType.java
@@ -2,5 +2,8 @@ package com.simplito.java.privmx_endpoint.model.events.eventSelectorTypes;
 
 /**
  * Implementations indicate the scope or area of the event you want to listen (subscribe) for.
+ *
+ * @category core
+ * @group EventSelectorTypes
  */
 public interface EventSelectorType { }
--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventTypes/EventType.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/events/eventTypes/EventType.java
@@ -2,6 +2,9 @@ package com.simplito.java.privmx_endpoint.model.events.eventTypes;
 
 /**
  * Implementations indicate the type of event you want to listen (subscribe) for.
+ *
+ * @category core
+ * @group EventTypes
  */
 public interface EventType {
 }
