914c418e23474a412c45cfb32625a72997a5e79c
Doominika <dsluzynska@simplito.com>
2025-10-14 14:16:57 +0200

    chore: add missing snippets to kvdb snippets

--- examples/privmx-snippets/src/main/java/Tools/Kvdbs/UsingKvdbs/ManagingEntries.java
+++ examples/privmx-snippets/src/main/java/Tools/Kvdbs/UsingKvdbs/ManagingEntries.java
@@ -1,8 +1,10 @@
 package Tools.Kvdbs.UsingKvdbs;
 
 import com.simplito.java.privmx_endpoint.model.KvdbEntry;
+import com.simplito.java.privmx_endpoint_extra.model.SortOrder;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class ManagingEntries extends ManagingKvdbs {
     void sendingEntries() {
@@ -10,8 +12,7 @@ public class ManagingEntries extends ManagingKvdbs {
         String kvdbEntryKey = "KVDB_ENTRY_KEY";
         byte[] privateMeta = "My private data".getBytes(StandardCharsets.UTF_8);
         byte[] publicMeta = "My public data".getBytes(StandardCharsets.UTF_8);
-        byte[] entryData = "ENTRY_DATA".getBytes();
-        byte[] newEntryData = "New Entry Data".getBytes();
+        byte[] entryData = "Entry Data".getBytes();
 
         // creating entry
         endpointSession.kvdbApi.setEntry(
@@ -27,15 +28,41 @@ public class ManagingEntries extends ManagingKvdbs {
                 kvdbID,
                 kvdbEntryKey
         );
+    }
 
-        // update created entry
+    void updateEntry() {
+        String kvdbID = "KVDB_ID";
+        String kvdbEntryKey = "KVDB_ENTRY_KEY";
+        byte[] newEntryData = "New Entry Data".getBytes();
+        KvdbEntry entry = endpointSession.kvdbApi.getEntry(kvdbID, kvdbEntryKey);
+
         endpointSession.kvdbApi.setEntry(
-                entry.info.kvdbId,
-                entry.info.key,
+                kvdbID,
+                kvdbEntryKey,
                 entry.publicMeta,
                 entry.privateMeta,
                 newEntryData,
                 entry.version
         );
+    }
+
+    void listEntries() {
+        String kvdbId = "KVDB_ID";
+        long startIndex = 0;
+        long pageSize = 30;
+
+        List<KvdbEntry> entries = endpointSession.kvdbApi.listEntries(
+                kvdbId,
+                startIndex,
+                pageSize,
+                SortOrder.DESC
+        ).readItems;
+    }
+
+    void deleteEntry() {
+        String kvdbID = "KVDB_ID";
+        String kvdbEntryKey = "KVDB_ENTRY_KEY";
+
+        endpointSession.kvdbApi.deleteEntry(kvdbID, kvdbEntryKey);
     }
 }
