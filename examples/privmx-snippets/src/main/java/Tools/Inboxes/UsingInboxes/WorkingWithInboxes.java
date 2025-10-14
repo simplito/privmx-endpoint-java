154180a941916129aa57a1c2ca45ef42a1055d6b
Doominika <dsluzynska@simplito.com>
2025-10-14 14:37:21 +0200

    chore: update snippet for retrieving public view of the inbox

--- examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/WorkingWithInboxes.java
+++ examples/privmx-snippets/src/main/java/Tools/Inboxes/UsingInboxes/WorkingWithInboxes.java
@@ -1,8 +1,5 @@
 package Tools.Inboxes.UsingInboxes;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.simplito.java.privmx_endpoint.model.Inbox;
 import com.simplito.java.privmx_endpoint.model.InboxPublicView;
 import com.simplito.java.privmx_endpoint.model.PagingList;
@@ -119,15 +116,7 @@ public class WorkingWithInboxes {
         String inboxID = "INBOX_ID";
 
         InboxPublicView inboxPublicView = endpointSession.inboxApi.getInboxPublicView(inboxID);
-
-        // This example uses com.google.code.gson:gson
-        // dependency for handling JSON objects in Java
-
-        // decoded publicMeta
-        String privateMeta = new String(inboxPublicView.publicMeta);
-        JsonObject privateMetaJson = JsonParser.parseString(privateMeta).getAsJsonObject();
-        JsonArray fieldsArray = privateMetaJson.getAsJsonArray("fields");
-        String question_1 = fieldsArray.get(0).getAsJsonObject().get("question").getAsString();
-        String question_2 = fieldsArray.get(1).getAsJsonObject().get("question").getAsString();
+        String publicMeta = new String(inboxPublicView.publicMeta, StandardCharsets.UTF_8);
+        System.out.println(publicMeta);
     }
 }
