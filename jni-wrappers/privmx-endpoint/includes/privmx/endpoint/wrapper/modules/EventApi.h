2c6115b16f53ff6e697030c1f378f606fd0c7dc9
Dawid Jenczewski <djenczewski@simplito.com>
2025-11-05 16:29:50 +0100

    chore: initial refactor project hierarchy prepared for multimodule publishing

---
+++ jni-wrappers/privmx-endpoint/includes/privmx/endpoint/wrapper/modules/EventApi.h
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include "../utils/utils.hpp"
+#include "Connection.h"
+#include "../parsers/parser.h"
+#include "privmx/endpoint/event/EventApi.hpp"
+
+using namespace privmx::endpoint;
+
+#ifndef PRIVMXENDPOINT_EVENTAPI_H
+#define PRIVMXENDPOINT_EVENTAPI_H
+
+privmx::endpoint::event::EventApi *getEventApi(JniContextUtils &ctx, jobject eventApiInstance);
+
+#endif //PRIVMXENDPOINT_EVENTAPI_H
---
+++ jni-wrappers/privmx-endpoint/includes/privmx/endpoint/wrapper/parsers/parser.h
@@ -0,0 +1,40 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef PRIVMX_POCKET_LIB_PARSER_H
+#define PRIVMX_POCKET_LIB_PARSER_H
+
+#include "../utils/utils.hpp"
+
+#include <jni.h>
+#include "model_native_initializers.h"
+
+std::vector<privmx::endpoint::core::UserWithPubKey>
+usersToVector(JniContextUtils &ctx, jobjectArray users);
+
+privmx::endpoint::core::PKIVerificationOptions
+parsePKIVerificationOptions(JniContextUtils &ctx, jobject pkiVerificationOptions);
+
+privmx::endpoint::core::ContainerPolicyWithoutItem
+parseContainerPolicyWithoutItem(JniContextUtils &ctx, jobject containerPolicyWithoutItem);
+
+privmx::endpoint::core::ContainerPolicy
+parseContainerPolicy(JniContextUtils &ctx, jobject containerPolicy);
+
+privmx::endpoint::core::ItemPolicy parseItemPolicy(JniContextUtils &ctx, jobject itemPolicy);
+
+privmx::endpoint::inbox::FilesConfig parseFilesConfig(JniContextUtils &ctx, jobject filesConfig);
+
+jobject parseEvent(JniContextUtils &ctx, std::shared_ptr<privmx::endpoint::core::Event> event);
+
+privmx::endpoint::core::PagingQuery parsePagingQuery(JniContextUtils &ctx, jobject pagingQuery);
+
+#endif //PRIVMX_POCKET_LIB_PARSER_H
