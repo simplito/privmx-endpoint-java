ccc65e153666c28e519ef7aa74779ff7c99ae0a4
Doominika <dsluzynska@simplito.com>
2025-12-01 11:32:10 +0100

    refactor: remove unnecessary getters

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/streams/RemoteStreamId.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/streams/RemoteStreamId.java
@@ -1,13 +1,9 @@
 package com.simplito.java.privmx_endpoint.model.streams;
 
 public class RemoteStreamId {
-    private final Long value;
+    public final Long value;
 
     public RemoteStreamId(Long value) {
         this.value = value;
-    }
-
-    public Long getValue() {
-        return value;
     }
 }
--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/streams/StreamHandle.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/streams/StreamHandle.java
@@ -1,13 +1,9 @@
 package com.simplito.java.privmx_endpoint.model.streams;
 
 public class StreamHandle {
-    private final Long value;
+    public final Long value;
 
     public StreamHandle(Long value) {
         this.value = value;
-    }
-
-    public Long getValue() {
-        return value;
     }
 }
