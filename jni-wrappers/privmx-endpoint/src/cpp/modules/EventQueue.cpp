b9c30d2c74dabd5f31d6c9829d1696f3730feab9
Doominika <dsluzynska@simplito.com>
2025-12-22 16:19:26 +0100

    refactor: move privmx-endpoint to jni-wrappers

---
+++ jni-wrappers/privmx-endpoint/src/cpp/modules/EventQueue.cpp
@@ -0,0 +1,64 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <jni.h>
+#include <privmx/endpoint/core/EventQueue.hpp>
+#include "privmx/endpoint/wrapper/utils/utils.hpp"
+#include "privmx/endpoint/wrapper/parsers/parser.h"
+
+using namespace privmx::endpoint::core;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_simplito_java_privmx_1endpoint_modules_core_EventQueue_emitBreakEvent(
+        JNIEnv *env,
+        jclass clazz
+) {
+    JniContextUtils ctx(env);
+    ctx.callVoidEndpointApi([]() {
+        EventQueue::getInstance().emitBreakEvent();
+    });
+}
+extern "C"
+JNIEXPORT jobject JNICALL
+Java_com_simplito_java_privmx_1endpoint_modules_core_EventQueue_waitEvent(
+        JNIEnv *env,
+        jclass clazz
+) {
+    JniContextUtils ctx(env);
+    jobject result;
+    ctx.callResultEndpointApi<jobject>(&result, [&ctx]() {
+        return parseEvent(ctx, EventQueue::getInstance().waitEvent().get());
+    });
+    if (ctx->ExceptionCheck()) {
+        return nullptr;
+    }
+    return result;
+}
+extern "C"
+JNIEXPORT jobject JNICALL
+Java_com_simplito_java_privmx_1endpoint_modules_core_EventQueue_getEvent(
+        JNIEnv *env,
+        jclass clazz
+) {
+    JniContextUtils ctx(env);
+    jobject result;
+    ctx.callResultEndpointApi<jobject>(&result, [&ctx]() {
+        auto eventHolder = EventQueue::getInstance().getEvent();
+        return !eventHolder.has_value() ?
+               nullptr :
+               parseEvent(ctx, eventHolder.value().get());
+    });
+    if (ctx->ExceptionCheck()) {
+        return nullptr;
+    }
+    return result;
+}
