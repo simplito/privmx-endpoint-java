5f151b2353e59ced5c1b65590f82baae806b6d2f
Doominika <dsluzynska@simplito.com>
2025-12-23 02:24:14 +0100

    feat: move files to jvm privmx-endpoint-streams

---
+++ privmx-endpoint-streams/jvm/src/main/java/com/simplito/java/privmx_endpoint/model/VideoTrackInfo.java
@@ -0,0 +1,20 @@
+package com.simplito.java.privmx_endpoint.model;
+
+import org.webrtc.RtpSender;
+import org.webrtc.VideoTrack;
+
+public class VideoTrackInfo {
+    public VideoTrack track;
+    public RtpSender sender;
+    public PmxFrameCryptor frameCryptor;
+
+    public VideoTrackInfo(
+            VideoTrack track,
+            RtpSender sender,
+            PmxFrameCryptor frameCryptor
+    ) {
+        this.track = track;
+        this.sender = sender;
+        this.frameCryptor = frameCryptor;
+    }
+}
