e9c70107a7d36c4b5c36b5ff2d49b8ada17281cf
Doominika <dsluzynska@simplito.com>
2026-02-10 12:36:07 +0100

    refactor: remove stream event types from base endpoint and put them into new streams directory

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/streams/eventSelectorTypes/StreamEventSelectorType.java
+++
@@ -1,7 +0,0 @@
-package com.simplito.java.privmx_endpoint.streams.eventSelectorTypes;
-
-public enum StreamEventSelectorType implements EventSelectorType {
-    CONTEXT_ID,
-    STREAMROOM_ID,
-    STREAM_ID
-}
