46f2e4ac4f3b7a53c6f48ae61527783308bd7d1b
Dawid Jenczewski <djenczewski@simplito.com>
2026-02-19 23:47:49 +0100

    chore: Import missing class

--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
+++ privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
@@ -33,6 +33,7 @@ import org.webrtc.audio.JavaAudioDeviceModule;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 //TODO: Good to remove context from StreamApi
