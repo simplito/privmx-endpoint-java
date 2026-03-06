c3a97c83df0f03e9b30b645f9a8e056bdb5eb145
Dawid Jenczewski <djenczewski@simplito.com>
2026-03-06 10:55:02 +0100

    refactor(privmx-endpoint-streams/android): remove duplicated types from events in android src

--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/model/events/StreamRoomDeletedEventData.java
+++
@@ -1,9 +0,0 @@
-package com.simplito.java.privmx_endpoint.model.events;
-
-public class StreamRoomDeletedEventData {
-    public final String streamRoomId;
-
-    public StreamRoomDeletedEventData(String streamRoomId) {
-        this.streamRoomId = streamRoomId;
-    }
-}
