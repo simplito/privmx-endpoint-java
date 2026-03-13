ae265dd2440aca843c1b070c19310cdf3d64f955
Dawid Jenczewski <djenczewski@simplito.com>
2026-03-13 12:11:39 +0100

    refactor(privmx-endpoint-streams/android): cleanup unused variables, remove todos and add nonNull annotation in StreamApi.java

--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
+++ privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
@@ -4,7 +4,6 @@ package com.simplito.java.privmx_endpoint.modules.stream;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.simplito.java.privmx_endpoint.model.ContainerPolicy;
 import com.simplito.java.privmx_endpoint.model.PagingList;
@@ -32,21 +31,11 @@ import org.webrtc.VideoTrack;
 import org.webrtc.audio.AudioDeviceModule;
 import org.webrtc.audio.JavaAudioDeviceModule;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
-//TODO: Good to remove context from StreamApi
 public class StreamApi {
-    public static final String VIDEO_TRACK_ID = "ARDAMSv0";
-    public static final String AUDIO_TRACK_ID = "ARDAMSa0";
-    public static final String VIDEO_TRACK_TYPE = "video";
-    private static final String TAG = "StreamApi";
-
-    private final Context appContext;
-    private final EglBase rootEglBase;
     private final StreamApiLow api;
     private final PeerConnectionManager pcManager;
     public final TrackFactory trackFactory;
@@ -84,28 +73,12 @@ public class StreamApi {
     }
 
     public StreamApi(
-            Context appContext,
-            EglBase rootEglBase,
-            StreamApiLow api
-    ) {
-        this(appContext, rootEglBase, api, null);
-    }
-
-    public StreamApi(
             @NonNull Context appContext,
             @NonNull EglBase rootEglBase,
-            @NonNull StreamApiLow api,
-            //TODO: remove this parameter
-            @Nullable PeerConnectionFactory pcFactory
+            @NonNull StreamApiLow api
     ) {
-        this.appContext = appContext;
-        this.rootEglBase = rootEglBase;
         this.api = api;
-        PeerConnectionFactory factory = pcFactory;
-        if (factory == null) {
-            //TODO: What should be passed to the options parameter
-            factory = DefaultPeerConnectionFactory(appContext, rootEglBase, new PeerConnectionFactory.Options());
-        }
+        PeerConnectionFactory factory = DefaultPeerConnectionFactory(appContext, rootEglBase, new PeerConnectionFactory.Options());
         pcManager = new PeerConnectionManager(
                 factory,
                 (sessionId, rtcConfiguration) -> {
@@ -172,7 +145,6 @@ public class StreamApi {
     public void joinStreamRoom(
             String streamRoomId
     ) {
-        //TODO: Rollback this change, it is do only for run test
         RoomJanusSession session = pcManager.createSession(streamRoomId);
         api.joinStreamRoom(streamRoomId, session.webrtc);
     }
@@ -203,7 +175,7 @@ public class StreamApi {
      * @throws IllegalStateException if call addTrack before call createStream
      */
     public void addTrack(
-            StreamHandle streamHandle,
+            @NonNull StreamHandle streamHandle,
             MediaStreamTrack track
     ) throws IllegalStateException {
         Objects.requireNonNull(streamHandle);
@@ -226,7 +198,7 @@ public class StreamApi {
     }
 
     public void setTrackObserver(
-            String roomId,
+            @NonNull String roomId,
             TrackObserver observer,
             String streamId
     ) {
@@ -238,7 +210,7 @@ public class StreamApi {
     }
 
     public void setConnectionStateObserver(
-            String roomId,
+            @NonNull String roomId,
             Consumer<PeerConnection.IceConnectionState> observer
     ) {
         Objects.requireNonNull(roomId);
@@ -261,8 +233,8 @@ public class StreamApi {
      * @throws IllegalStateException when Stream with this StreamHandle doesn't exist.
      */
     public void removeTrack(
-            StreamHandle streamHandle,
-            MediaStreamTrack track
+            @NonNull StreamHandle streamHandle,
+            @NonNull MediaStreamTrack track
     ) throws IllegalStateException {
         Objects.requireNonNull(streamHandle);
         RoomJanusSession session = pcManager.getSession(streamHandle);
@@ -278,17 +250,17 @@ public class StreamApi {
         }
     }
 
-    public StreamPublishResult publishStream(StreamHandle streamHandle) {
+    public StreamPublishResult publishStream(@NonNull StreamHandle streamHandle) {
         Objects.requireNonNull(streamHandle);
         return api.publishStream(streamHandle);
     }
 
-    public StreamPublishResult updateStream(StreamHandle streamHandle) {
+    public StreamPublishResult updateStream(@NonNull StreamHandle streamHandle) {
         Objects.requireNonNull(streamHandle);
         return api.updateStream(streamHandle);
     }
 
-    public void unpublishStream(StreamHandle streamHandle) {
+    public void unpublishStream(@NonNull StreamHandle streamHandle) {
         Objects.requireNonNull(streamHandle);
         api.unpublishStream(streamHandle);
     }
