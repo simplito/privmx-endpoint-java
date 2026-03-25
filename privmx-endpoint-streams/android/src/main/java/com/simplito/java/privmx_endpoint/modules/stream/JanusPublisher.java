9aa97b24bd6df40645c97166e4dfd8b6b27bb565
Dawid Jenczewski <djenczewski@simplito.com>
2026-03-25 16:30:35 +0100

    feat(privmx-endpoint-streams/android): WIP implement creating DataChannel with waiting for open in JanusPublisher.java

--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/JanusPublisher.java
+++ privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/JanusPublisher.java
@@ -1,7 +1,6 @@
 package com.simplito.java.privmx_endpoint.modules.stream;
 
-import androidx.annotation.Nullable;
-
+import androidx.annotation.NonNull;
 import com.simplito.java.privmx_endpoint.model.AudioTrackInfo;
 import com.simplito.java.privmx_endpoint.model.ConnectionType;
 import com.simplito.java.privmx_endpoint.model.stream.SdpWithTypeModel;
@@ -11,15 +10,14 @@ import org.webrtc.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-public class JanusPublisher extends JanusConnection{
+public class JanusPublisher extends JanusConnection {
     private final Map<String, AudioTrackInfo> audioTracks = new HashMap<>();
     private final Map<String, VideoTrackInfo> videoTracks = new HashMap<>();
+    private DataChannel dataChannel = null;
     private final BiConsumer<Long, SdpWithTypeModel> setNewOfferOnReconfigure;
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
@@ -36,7 +34,7 @@ public class JanusPublisher extends JanusConnection{
     }
 
     public void addAudioTrack(org.webrtc.AudioTrack audioTrack) {
-        synchronized (audioTracks) {
+        synchronized (peerConnection) {
             RtpSender rtpSender2 = peerConnection.addTrack(audioTrack);
             PmxFrameCryptor frameCryptor = PmxFrameCryptorFactory.createPmxFrameCryptorFromRtpSender(
                     peerConnectionFactory,
@@ -58,7 +56,7 @@ public class JanusPublisher extends JanusConnection{
 
     public void addVideoTrack(org.webrtc.VideoTrack videoTrack) {
         if (peerConnectionFactory != null) {
-            synchronized (videoTracks) {
+            synchronized (peerConnection) {
                 RtpSender rtpSender = peerConnection.addTrack(videoTrack);
                 PmxFrameCryptor frameCryptor = PmxFrameCryptorFactory.createPmxFrameCryptorFromRtpSender(
                         peerConnectionFactory,
@@ -79,6 +77,74 @@ public class JanusPublisher extends JanusConnection{
         }
     }
 
+    @NonNull
+    public synchronized DataChannel getOrCreateDataChannel() throws RuntimeException{
+        if (dataChannel != null) {
+            if (dataChannel.state() == DataChannel.State.OPEN) return dataChannel;
+            //TODO: try reconnecting
+            if (dataChannel.state() == DataChannel.State.CLOSING)
+                throw new IllegalStateException("Data channel is now closing");
+            //If state is closed then create new data channel and wait for open
+            //If datachannel state is connecting then method execution waits on synchronized block.
+        }
+        if (dataChannel == null || dataChannel.state() == DataChannel.State.CLOSED) {
+            dataChannel = createNewDataChannel();
+        }
+        waitForOpen(dataChannel,false);
+        if(dataChannel.state() != DataChannel.State.OPEN) throw new RuntimeException("Cannot open data channel");
+        return dataChannel;
+    }
+
+    private DataChannel createNewDataChannel() {
+        synchronized (peerConnection) {
+            return peerConnection.createDataChannel("JanusDataChannel", getDataChannelInit());
+        }
+    }
+
+    private void waitForOpen(DataChannel channel, boolean withTimeout) {
+        if (channel.state() == DataChannel.State.OPEN) return;
+        if (channel.state() != DataChannel.State.CONNECTING) throw new RuntimeException("Channel is closing or closed");
+        CompletableFuture<DataChannel> dataChannelCompletableFuture = new CompletableFuture<>();
+        channel.registerObserver(new DataChannel.Observer() {
+            @Override
+            public void onBufferedAmountChange(long l) {
+            }
+
+            @Override
+            public void onStateChange() {
+                System.out.println("datachannel state updated: " + channel.state().name());
+                if (channel.state() == DataChannel.State.OPEN) {
+                    dataChannelCompletableFuture.complete(channel);
+                } else {
+                    dataChannelCompletableFuture.completeExceptionally(new RuntimeException("Cannot open data channel"));
+                }
+            }
+
+            @Override
+            public void onMessage(DataChannel.Buffer buffer) {
+            }
+        });
+        try {
+            if (withTimeout) {
+                dataChannelCompletableFuture.get(20, TimeUnit.SECONDS);
+                return;
+            }
+            dataChannelCompletableFuture.get();
+        }catch (Throwable ignored){}
+    }
+
+    private void closeDataChannel(){
+        if(dataChannel != null && dataChannel.state() != DataChannel.State.CLOSING && dataChannel.state() != DataChannel.State.CLOSING) dataChannel.dispose();
+        dataChannel = null;
+    }
+
+    private DataChannel.Init getDataChannelInit() {
+        DataChannel.Init init = new DataChannel.Init();
+        init.ordered = true;
+        init.negotiated = false;
+        return init;
+    }
+
     public void removeAudioTrack(String id) {
         synchronized (audioTracks) {
             AudioTrackInfo audioTrackInfo = audioTracks.get(id);
@@ -97,20 +163,25 @@ public class JanusPublisher extends JanusConnection{
         }
     }
 
-    public synchronized String createOffer(){
-        CompletableFuture<SessionDescription> res = new CompletableFuture<>();
-        peerConnection.createOffer(new SdpObserver(res), new MediaConstraints());
-        try {
-            SessionDescription offer = res.get();
-            peerConnection.setLocalDescription(new SdpObserver(null),offer);
-            return offer.description;
-        }catch (Exception e){
-            throw new RuntimeException("Cannot create offer");
+    public String createOffer() {
+        synchronized (peerConnection) {
+            CompletableFuture<SessionDescription> res = new CompletableFuture<>();
+            peerConnection.createOffer(new SdpObserver(res), new MediaConstraints());
+            try {
+                SessionDescription offer = res.get();
+                peerConnection.setLocalDescription(new SdpObserver(null), offer);
+                System.out.println("publisher Created offer " + offer.description);
+                return offer.description;
+            } catch (Exception e) {
+                throw new RuntimeException("Cannot create offer");
+            }
         }
     }
 
-    public synchronized void setAnswer(String sdp, String type){
-        peerConnection.setRemoteDescription(new SdpObserver(null),new SessionDescription(SessionDescription.Type.fromCanonicalForm(type),sdp));
+    public void setAnswer(String sdp, String type) {
+        synchronized (peerConnection) {
+            peerConnection.setRemoteDescription(new SdpObserver(null), new SessionDescription(SessionDescription.Type.fromCanonicalForm(type), sdp));
+        }
     }
 
 
@@ -123,10 +194,10 @@ public class JanusPublisher extends JanusConnection{
 
     @Override
     public void setFrameCryptorOptions(PmxFrameCryptor.PmxFrameCryptorOptions options) {
-        this.videoTracks.values().forEach(it ->{
+        this.videoTracks.values().forEach(it -> {
             it.frameCryptor.setOptions(options);
         });
-        this.audioTracks.values().forEach(it ->{
+        this.audioTracks.values().forEach(it -> {
             it.frameCryptor.setOptions(options);
         });
     }
@@ -134,9 +205,9 @@ public class JanusPublisher extends JanusConnection{
 
     @Override
     public void onRenegotiationNeeded() {
-        if(getSessionId() > -1) {
-            executorService.execute(()->{
-                setNewOfferOnReconfigure.accept(getSessionId(),new SdpWithTypeModel(createOffer(), SessionDescription.Type.OFFER.canonicalForm()));
+        if (getSessionId() > -1) {
+            executorService.execute(() -> {
+                setNewOfferOnReconfigure.accept(getSessionId(), new SdpWithTypeModel(createOffer(), SessionDescription.Type.OFFER.canonicalForm()));
             });
         }
     }
--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/RemoteStreamObserver.java
+++ privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/RemoteStreamObserver.java
@@ -4,5 +4,6 @@ import org.webrtc.MediaStreamTrack;
 
 public interface RemoteStreamObserver {
     void OnRemoteTrack(String streamId, MediaStreamTrack track);
+    //TODO: Maybe replace byte[] with ByteBuffer then developer should read data from it. This can optimize memory usage.
     void OnRemoteData(String streamId, byte[] msg);
 }
