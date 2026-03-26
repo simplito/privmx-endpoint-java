127e55bbf3d08509e8aa3defff58113bcbf54dec
Doominika <dsluzynska@simplito.com>
2026-03-26 14:25:12 +0100

    docs(privmx-endpoint-streams/android): add description for createStream method

--- privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
+++ privmx-endpoint-streams/android/src/main/java/com/simplito/java/privmx_endpoint/modules/stream/StreamApi.java
@@ -310,6 +310,16 @@ public class StreamApi implements AutoCloseable {
         api.leaveStreamRoom(streamRoomId);
     }
 
+    /**
+     * Creates a local stream handle for publishing media in given Stream Room.
+     * <p>
+     * {@link #joinStreamRoom(String)} must be called before this method.
+     * Use {@link #addTrack(StreamHandle, MediaStreamTrack)} to add media tracks
+     * before calling {@link #publishStream(StreamHandle)}.
+     *
+     * @param streamRoomId ID of the Stream Room to create the stream in
+     * @return handle to the local stream instance
+     */
     public StreamHandle createStream(String streamRoomId) {
         RoomJanusSession session = pcManager.getSession(streamRoomId);
         if (session == null)
