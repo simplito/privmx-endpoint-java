2f5ded5fa5d8ab849ee53edddf901f8e661b0556
Doominika <dsluzynska@simplito.com>
2026-04-16 14:34:46 +0200

    docs: add docs for StreamEventData

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/events/StreamEventData.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/events/StreamEventData.java
@@ -2,11 +2,29 @@ package com.simplito.java.privmx_endpoint.model.stream.events;
 
 import java.util.List;
 
+/**
+ * Data describing the event that occurs when a participant joins a StreamRoom.
+ */
 public class StreamEventData {
+    /**
+     * Identifier of the stream room that was joined
+     */
     public String streamRoomId;
+
     public List<Long> streamIds;
+
+    /**
+     * Identifier of the user who joined the StreamRoom
+     */
     public String userId;
 
+    /**
+     * Constructs a new {@link StreamEventData} instance.
+     *
+     * @param streamRoomId Identifier of the stream room that was joined
+     * @param streamIds
+     * @param userId       Identifier of the user who joined the StreamRoom
+     */
     public StreamEventData(String streamRoomId, List<Long> streamIds, String userId) {
         this.streamRoomId = streamRoomId;
         this.streamIds = streamIds;
