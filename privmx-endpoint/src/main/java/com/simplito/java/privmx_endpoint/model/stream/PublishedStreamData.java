0a0170b9913b0d3d4c15252cee6a45a0b8277361
Doominika <dsluzynska@simplito.com>
2026-04-16 14:21:56 +0200

    docs: add docs for PublishedStreamData

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/PublishedStreamData.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/PublishedStreamData.java
@@ -1,10 +1,31 @@
 package com.simplito.java.privmx_endpoint.model.stream;
 
+/**
+ * Represents data of a published stream.
+ */
 public class PublishedStreamData {
+    /**
+     * Identifier of the room in which the stream is published
+     */
     public String streamRoomId;
+
+    /**
+     * Information about the published stream
+     */
     public StreamInfo stream;
+
+    /**
+     * Identifier of the user who published the stream
+     */
     public String userId;
 
+    /**
+     * Constructs a new {@link PublishedStreamData} instance.
+     *
+     * @param streamRoomId Identifier of the room in which the stream is published
+     * @param stream       Information about the published stream
+     * @param userId       Identifier of the user who published the stream
+     */
     public PublishedStreamData(String streamRoomId, StreamInfo stream, String userId) {
         this.streamRoomId = streamRoomId;
         this.stream = stream;
