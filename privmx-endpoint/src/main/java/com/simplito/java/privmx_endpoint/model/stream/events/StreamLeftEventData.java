f631d5bf5e28ac5d660ef3a104084825dd2ec2fb
Doominika <dsluzynska@simplito.com>
2026-04-16 14:33:43 +0200

    docs: add docs for StreamLeftEventData

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/events/StreamLeftEventData.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/events/StreamLeftEventData.java
@@ -1,12 +1,30 @@
 package com.simplito.java.privmx_endpoint.model.stream.events;
 
-import java.util.List;
-
+/**
+ * Data describing the event that occurs when a participant leaves a StreamRoom.
+ */
 public class StreamLeftEventData {
+    /**
+     * Identifier of the StreamRoom that was left
+     */
     public String streamRoomId;
+
+    /**
+     * Identifier of the stream that was left
+     */
     public Long streamId;
+
+    /**
+     * Identifier of the user who left the StreamRoom
+     */
     public String userId;
 
+    /**
+     * Constructs a new {@link StreamLeftEventData} instance.
+     *
+     * @param streamRoomId Identifier of the StreamRoom that was left
+     * @param streamId     Identifier of the stream that was left
+     */
     public StreamLeftEventData(String streamRoomId, Long streamId, String userId) {
         this.streamRoomId = streamRoomId;
         this.streamId = streamId;
