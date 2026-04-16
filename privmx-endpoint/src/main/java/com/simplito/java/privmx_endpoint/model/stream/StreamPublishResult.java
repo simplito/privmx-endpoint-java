da9b818d7e714f8d88c2f667930703b142b8e9c2
Doominika <dsluzynska@simplito.com>
2026-04-16 14:16:26 +0200

    docs: add docs for StreamPublishResult

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamPublishResult.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamPublishResult.java
@@ -1,12 +1,34 @@
 package com.simplito.java.privmx_endpoint.model.stream;
 
+/**
+ * Represents the result of a stream publish/update operation.
+ */
 public class StreamPublishResult {
-public Boolean published;
-public PublishedStreamData data;
+    /**
+     * Indicates if the stream was successfully published
+     */
+    public Boolean published;
 
+    /**
+     * Additional information about the published stream
+     */
+    public PublishedStreamData data;
+
+    /**
+     * Constructs a new {@link StreamPublishResult} instance.
+     *
+     * @param published Indicates if the stream was successfully published
+     */
     public StreamPublishResult(Boolean published) {
         this(published, null);
     }
+
+    /**
+     * Constructs a new {@link StreamPublishResult} instance.
+     *
+     * @param published Indicates if the stream was successfully published
+     * @param data      Additional information about the published stream
+     */
     public StreamPublishResult(Boolean published, PublishedStreamData data) {
         this.published = published;
         this.data = data;
