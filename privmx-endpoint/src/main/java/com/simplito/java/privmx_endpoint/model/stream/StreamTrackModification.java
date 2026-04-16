3c89f07696b4979a983f0ff1a84efa3f6b853501
Doominika <dsluzynska@simplito.com>
2026-04-16 13:57:24 +0200

    docs: add docs for StreamTrackModification

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamTrackModification.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamTrackModification.java
@@ -2,10 +2,26 @@ package com.simplito.java.privmx_endpoint.model.stream;
 
 import java.util.List;
 
+/**
+ * Represents a set of track modifications applied to a specific stream.
+ */
 public class StreamTrackModification {
+    /**
+     * Identifier of the stream for which tracks were modified
+     */
     public Long streamId;
+
+    /**
+     * List of track modifications, each containing the state of a track before and after the change
+     */
     public List<StreamTrackModificationPair> tracks;
 
+    /**
+     * Constructs a new {@link StreamTrackModification} instance.
+     *
+     * @param streamId Identifier of the stream for which tracks were modified
+     * @param tracks   List of track modifications, each containing the state of a track before and after the change
+     */
     public StreamTrackModification(Long streamId, List<StreamTrackModificationPair> tracks) {
         this.streamId = streamId;
         this.tracks = tracks;
