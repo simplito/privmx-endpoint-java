6989013ee797c3fbfc7dbd6b6c2c46f356e807b6
Doominika <dsluzynska@simplito.com>
2026-04-16 14:19:59 +0200

    docs: add docs for StreamHandle

--- privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamHandle.java
+++ privmx-endpoint/src/main/java/com/simplito/java/privmx_endpoint/model/stream/StreamHandle.java
@@ -1,12 +1,26 @@
 package com.simplito.java.privmx_endpoint.model.stream;
 
+/**
+ * Unique handle to a stream, used to perform operations on the stream.
+ */
 public class StreamHandle {
+    /**
+     * The value of the {@link StreamHandle}.
+     */
     private final Long value;
 
+    /**
+     * Constructs a new {@link StreamHandle} instance.
+     *
+     * @param value The value of the {@link StreamHandle}.
+     */
     public StreamHandle(Long value) {
         this.value = value;
     }
 
+    /**
+     * Gets the value of the {@link StreamHandle}.
+     */
     public Long getValue() {
         return value;
     }
